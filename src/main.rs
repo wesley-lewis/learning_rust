65721b56cfb13fa26675443e0f2e88b20be3b6a9
Wesley Lewis <lewis.wes.23@gmail.com>
2022-07-23 17:24:20 +0530

    Finished Chap 2

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("The game is simple.\nYou have to just the guess the random number which is being created.\nThe random created will be in the range of 0 to 100.\n\nBest of Luck!\n\n");
+    
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // println!("The secret number is {}", secret_number);
+
+    loop {
+        println!("Please input your guess: ");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("You entered a String.\nPlease enter a number!\n");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
